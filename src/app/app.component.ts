cedf3cb0071f94ba400161aee3c443397617c4a2
TwistedWords <bf.botezatu@gmail.com>
2023-10-04 23:27:02 +0300

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'list';
+
+  newTask: string = '';
+  incompleteTasks: string[] = [];
+  completeTasks: string[] = [];
+
+
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      this.incompleteTasks.push(this.newTask);
+      this.newTask = '';
+    }
+
+    console.log(this.incompleteTasks);
+    
+  }
+
+  completeTask(task: string) {
+    const index = this.incompleteTasks.indexOf(task);
+    if (index !== -1) {
+      this.incompleteTasks.splice(index, 1);
+      this.completeTasks.push(task);       
+    }
+  }
+
+  incompleteTask(task: string) {
+    const index = this.completeTasks.indexOf(task);
+    if (index !== -1) {
+      this.completeTasks.splice(index, 1);
+      this.incompleteTasks.push(task);       
+    }
+  }
+
+  deleteIncompleteTask(index: number) {
+    if (index >= 0 && index < this.incompleteTasks.length) {
+      this.incompleteTasks.splice(index, 1);
+    }
+  }
+
+  deleteCompleteTask(index: number) {
+    if (index >= 0 && index < this.completeTasks.length) {
+      this.completeTasks.splice(index, 1); 
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
